cc81f3908bc0a35c1bf357fa89c074fcb56640e7
Ben <bmsteve96@gmail.com>
2023-02-26 13:12:01 +0300

    migrate cra to vite in example

---
+++ example/src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PackageName from "@flexcodelabs/package-name";
+
+
+const App = () => {
+  return <div>
+    <PackageName />
+  </div>;
+};
+
+export default App;
